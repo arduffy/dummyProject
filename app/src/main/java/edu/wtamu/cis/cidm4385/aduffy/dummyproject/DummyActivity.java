1c020e90b221854416c42e457d0b1a165c94cab1
Amanda <arduffy>
2018-03-19 19:03:22 -0500

    added comment in DummyActivity

--- app/src/main/java/edu/wtamu/cis/cidm4385/aduffy/dummyproject/DummyActivity.java
+++ app/src/main/java/edu/wtamu/cis/cidm4385/aduffy/dummyproject/DummyActivity.java
@@ -9,5 +9,7 @@ public class DummyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dummy);
+
+        //Amanda's comment for demonstrating branch/merge
     }
 }
