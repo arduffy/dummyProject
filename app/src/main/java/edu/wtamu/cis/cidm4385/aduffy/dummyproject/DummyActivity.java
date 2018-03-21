7f066f8055ed0b7bcc7b5487dadd003c0f1a7adc
Haden McAfee <hadenmcafee@gmail.com>
2018-03-20 21:56:14 -0500

    I made some sweet changes!

--- app/src/main/java/edu/wtamu/cis/cidm4385/aduffy/dummyproject/DummyActivity.java
+++ app/src/main/java/edu/wtamu/cis/cidm4385/aduffy/dummyproject/DummyActivity.java
@@ -3,8 +3,9 @@ package edu.wtamu.cis.cidm4385.aduffy.dummyproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//we really ought to do something with this activity... and is it right to call it a dummy?
 public class DummyActivity extends AppCompatActivity {
-
+    //it might even need counseling to get over our cruel ridicule
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
