db8cf0b70debb553567a2235dd6dbfddc62e9432
HEYOU <slseethoff1@buffs.wtamu.edu>
2018-03-20 22:55:50 -0500

    My Branch

--- app/src/main/java/edu/wtamu/cis/cidm4385/aduffy/dummyproject/DummyActivity.java
+++ app/src/main/java/edu/wtamu/cis/cidm4385/aduffy/dummyproject/DummyActivity.java
@@ -9,5 +9,7 @@ public class DummyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dummy);
+
+        //Scotty's comment
     }
 }
